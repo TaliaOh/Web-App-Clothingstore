343093f5d3b7ae9e30ac6ba6a06cf1c1847d0670
danielmishan85 <danielmishan85@gmail.com>
2023-01-02 17:09:36 +0200

    continue the product route and add a error model

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,42 +1,49 @@
 const express = require('express');
 
+const HttpError = require('../models/http-error');
+
 const router = express.Router();
 
 const DUMMY_DATA = [
-    {
-        id: 'p1',
-        productName: 'T-shirt',
-        price: '20$',
-        color: 'black',
-        image: ''
-    },
-    {
-        id: 'p2',
-        productName: 'T-shirt',
-        price: '20$',
-        color: 'white',
-        image: ''
-    },
-    {
-        id: 'p3',
-        productName: 'T-shirt',
-        price: '20$',
-        color: 'gray',
-        image: ''
-    }
-]
+  {
+    id: 'p1',
+    productName: 'T-shirt',
+    price: '20$',
+    color: 'black',
+    image: '',
+  },
+  {
+    id: 'p2',
+    productName: 'T-shirt',
+    price: '20$',
+    color: 'white',
+    image: '',
+  },
+  {
+    id: 'p3',
+    productName: 'T-shirt',
+    price: '20$',
+    color: 'gray',
+    image: '',
+  },
+];
 
 router.get('/', (req, res) => {
   console.log('Get Request in Places');
   res.json({ message: 'It Words!' });
 });
 
-router.get('/:pid', (req, res) => {
-    const productId = req.params.pid
-    const product = DUMMY_DATA.find(p => {
-        return p.id === productId
-    })
-    res.json({ product });
+router.get('/:pid', (req, res, next) => {
+  const productId = req.params.pid;
+  const product = DUMMY_DATA.find((p) => {
+    return p.id === productId;
   });
+  if (!product) {
+    return next(
+      new HttpError('Could not find a product for the provided id.', 404)
+    );
+  }
+  res.json({ product });
+});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const productsRoutes = require('./routes/products')
+const productsRoutes = require('./routes/products');
 
 const server = express();
 
 server.use('/api/products', productsRoutes);
+
+server.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
+});
 
 server.listen(5000);
