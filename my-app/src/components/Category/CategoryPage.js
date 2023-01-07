8c6f27f1622794d6fc9f8dba3149ea659c0ef38e
TaliaOh <taloh1503@gmail.com>
2023-01-07 17:13:31 +0200

    category Page

--- my-app/src/components/Category/CategoryPage.js
+++ my-app/src/components/Category/CategoryPage.js
@@ -1,20 +1,14 @@
 import React from "react";
 import products from "../../data";
-import TitleDivider from "../Ui/TitleDivider";
-import Container from "@mui/system";
-import ProductCard from "../Products/ProductCard";
-import Grid from "@mui/material";
 import CategoryCard from "./CategoryCard";
-const CategoryPage = () => {
-  const categories = [...new Set(products.map((product) => product.category))];
+import CategorySection from "./CategorySection";
 
+const CategoryPage = () => {
   return (
     <div>
       <h1>Categories</h1>
 
-      {categories.map((category) => (
-        <li key={category}>{category}</li>
-      ))}
+      <CategorySection></CategorySection>
     </div>
   );
 };
